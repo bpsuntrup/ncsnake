49b4ffd1b08cde6600210fff524573db0834ce67
Benjamin Suntrup <bpsuntrup@outlook.com>
2018-08-06 23:21:35 -0500

    add utils

--- ncsnake.py
+++ ncsnake.py
@@ -1,32 +1,8 @@
 from enum import Enum
 from itertools import chain
 from operator import add
+from utils import NCurses, apply, chomp
 
-# Utils:
-def chomp(iter):
-    """ Returns an iterator sans the last item in iter
-    TODO: test this """
-
-    # Get first element 
-    try:
-        first = next(iter)
-    except StopIteration:
-        raise StopIteration
-
-    # Get second element
-    try:
-        second = next(iter)
-    except StopIteration:
-        # Don't yield anything if there was only one element
-        raise StopIteration
-
-    # Safe now to yield first
-    yield first
-
-    while True:
-        first = second
-        second = next(iter)
-        yield first
 
 class Direction(Enum):
     """ Use the values in this direction type to calculate the next position
@@ -36,10 +12,29 @@ class Direction(Enum):
     EAST = (0, 1)
     WEST = (0, -1)
 
-def advance(snake, direction):
+def advance(snake, direction, food):
     ''' Takes a list of positions (snake) and returns a snake advanced in the
-    direction given of type Direction'''
-    return chain([tuple(map(add, snake[0], direction))], chomp(iter(snake)))
+    direction given of type Direction. Snake "grows" if it eats food'''
+    return \
+    apply(
+        lambda x:
+            chain(
+                [x],
+                chomp(iter(snake)) if not x == food else iter(snake)),
+        tuple(
+            map(
+                add,
+                snake[0],
+                direction.value)))
+
+def check_snake(snake, direction, boundary):
+    ''' Returns true if snake may proceed in given direction without eating
+    itself or falling off the world, False otherwise'''
+    return \
+    not apply(
+        lambda x: x in snake or x in boundary,
+        tuple(map(add, snake[0], direction.value)))
+
     
 if __name__ == '__main__':
     for i in chomp(iter([1,2,3,4,5])):
@@ -52,7 +47,7 @@ if __name__ == '__main__':
     snake = [(0,0), (0,1), (0,2)]
     list(map(print, snake))
     list(map(print, chomp(iter(snake))))
-    list(map(print, advance(snake, (0,1))))
+    list(map(print, advance(snake, Direction.EAST, (0,1))))
     print('mapaddsnake0direction:')
     print(tuple(map(add,snake[0],(0,1))))
 
---
+++ utils.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+import curses
+
+def apply(f, *args):
+    return f(*args)
+
+def chomp(iter):
+    """ Returns an iterator sans the last item in iter
+    TODO: test this """
+
+    # Get first element 
+    try:
+        first = next(iter)
+    except StopIteration:
+        raise StopIteration
+
+    # Get second element
+    try:
+        second = next(iter)
+    except StopIteration:
+        # Don't yield anything if there was only one element
+        raise StopIteration
+
+    # Safe now to yield first
+    yield first
+
+    while True:
+        first = second
+        second = next(iter)
+        yield first
+
+
+class NCurses(object):
+    """ncurses context manager"""
+    def __init__(self):
+        self.stdscr = curses.initscr()
+        curses.start_color()
+        curses.noecho()
+        curses.cbreak()
+        self.stdscr.keypad(1)
+        curses.curs_set(0)
+    def __enter__(self):
+        return curses
+
+    def __exit__(self, *args):
+        curses.nocbreak(); self.stdscr.keypad(0); curses.echo()
+        curses.endwin()
+
+if __name__ == "__main__":
+    import time
+    with NCurses() as nc:
+        # Pick colors I like:
+        nc.init_pair(1, nc.COLOR_RED, nc.COLOR_WHITE)
+        nc.init_pair(2, nc.COLOR_WHITE, nc.COLOR_RED)
+        nc.init_pair(3, nc.COLOR_BLUE, nc.COLOR_WHITE)
+        nc.init_pair(4, nc.COLOR_BLUE, nc.COLOR_BLUE)
+        nc.init_pair(5, nc.COLOR_GREEN, nc.COLOR_BLACK)
+        nc.init_pair(6, nc.COLOR_BLACK, nc.COLOR_WHITE)
+
+        # Draw a new window... I guess
+        x = 0
+        y = 0
+        height = 40
+        width = 40
+        win = nc.newwin(height, width, y, x)
+        win.timeout(200)
+        win.addstr(2, 20, 'READY', nc.color_pair(3))
+        win.refresh()
+        time.sleep(2)
+        win.addstr(4,20,'GO', nc.color_pair(2))
+        win.refresh()
+
+        
